384c3e120d9f6ddfd482e68cac4ce697b6f79071
Giselle <rodriguezdelgadogiselle@gmail.com>
2023-12-05 16:40:23 -0500

    first commit

---
+++ src/components/Events/EventDetails.jsx
@@ -0,0 +1,137 @@
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
+
+import Header from "../Header.jsx";
+import { fetchEvent, deleteEvent } from "../../util/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import { useState } from "react";
+import Modal from "../UI/Modal.jsx";
+
+export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorDeleting,
+    error: deleteError,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      navigate("/events");
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none",
+      });
+    },
+  });
+
+  function handleStartDelete() {
+    setIsDeleting(true);
+  }
+  function handlerStopDelete() {
+    setIsDeleting(false);
+  }
+  function deleteHandler() {
+    mutate({ id: id });
+  }
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events."}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <article id="event-details">
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleStartDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`https://backend-6ihlhmycoa-ue.a.run.app/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {new Date(data.date).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}{" "}
+                @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </article>
+    );
+  }
+
+  return (
+    <>
+      {isDeleting && (
+        <Modal onClose={handlerStopDelete}>
+          <h2>Are you sure?</h2>
+          <p>
+            Do you really want to delete this event? This action cannot be
+            undone.
+          </p>
+          <div className="form-actions">
+            {isPendingDeletion && <p>Deleting, please wait...</p>}
+            {!isPendingDeletion && (
+              <>
+                {" "}
+                <button onClick={handlerStopDelete} className="button-text">
+                  Cancel
+                </button>
+                <button onClick={deleteHandler} className="button">
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+          {isErrorDeleting && (
+            <ErrorBlock
+              title="Failed to delete event"
+              message={
+                deleteError.info?.message ||
+                "Failed to delete event, please try again later."
+              }
+            ></ErrorBlock>
+          )}
+        </Modal>
+      )}
+
+      <Outlet />
+      <Header>
+        <Link to="/events" className="nav-item">
+          View all Events
+        </Link>
+      </Header>
+      {content}
+    </>
+  );
+}
